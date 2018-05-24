149c3269f4dcfc0347812cbed73762bf3e552151
Vitor Dullens <vitordullens2.0@gmail.com>
2018-05-24 00:07:18 -0300

    adding training branch

---
+++ django-training/contas/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+import datetime
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>What time is it? %s. </body></html>" % now
+    return HttpResponse(html)
+
+def index(request):
+    data = {}
+    data['now'] = datetime.datetime.now()
+    return render(request, 'contas/index.html', data)
--- django-training/controle_gastos/urls.py
+++ django-training/controle_gastos/urls.py
@@ -1,4 +1,4 @@
-"""project URL Configuration
+"""controle_gastos URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -14,14 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from contas.views import current_datetime, index
 
 urlpatterns = [
-    path('', include('start.urls')),
     path('admin/', admin.site.urls),
+    path('', index),
+    path('date/', current_datetime)
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
