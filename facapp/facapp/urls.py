718f15ac8031f2f0a997b016806566225aa76efa
Vitor Dullens <vitordullens2.0@gmail.com>
2018-05-25 23:29:02 -0300

    A hole new world

--- facapp/facapp/urls.py
+++ facapp/facapp/urls.py
@@ -1,4 +1,4 @@
-"""project URL Configuration
+"""facapp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -14,14 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
 
 urlpatterns = [
-    path('', include('start.urls')),
     path('admin/', admin.site.urls),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
