9b606d146482e4c462fb3c15cd43e0895a07252d
Vitor Dullens <vitordullens2.0@gmail.com>
2018-05-25 21:47:25 -0300

    adding update and remove options

--- django-training/contas/views.py
+++ django-training/contas/views.py
@@ -22,8 +22,26 @@ def listagem(request):
     return render(request, 'contas/listagem.html', data)
 
 def nova_transacao(request):
+    data = {}
     form = TransacaoForm(request.POST or None)
     if form.is_valid():
         form.save()
         return redirect('url_listagem')
-    return render(request, 'contas/form.html', { 'form':form })
+    data['form'] = form
+    return render(request, 'contas/form.html', data)
+
+def update(request, pk):
+    data = {}
+    transacao = Transacao.objects.get(pk=pk)
+    form = TransacaoForm(request.POST or None, instance=transacao)
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+    data['form'] = form
+    data['transacao'] = transacao
+    return render(request, 'contas/form.html', data)
+
+def delete(request, pk):
+    transacao = Transacao.objects.get(pk=pk)
+    transacao.delete()
+    return redirect('url_listagem')
--- django-training/controle_gastos/urls.py
+++ django-training/controle_gastos/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from contas.views import current_datetime, index, listagem, nova_transacao
+from contas.views import current_datetime, index, listagem, nova_transacao, update, delete
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', index),
     path('date/', current_datetime),
+    path('update/<int:pk>', update, name='url_update'),
+    path('delete/<int:pk>', delete, name='url_delete'),    
     path('nova/', nova_transacao, name='url_nova'),
     path('', listagem, name='url_listagem'),
 ]
