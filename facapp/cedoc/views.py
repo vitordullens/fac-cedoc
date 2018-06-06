d839ff1d7cd70540d8d19b5016a415a9077030b5
Vitor Dullens <vitordullens2.0@gmail.com>
2018-06-05 23:43:58 -0300

    deleting file as well (only text for now)

--- facapp/cedoc/views.py
+++ facapp/cedoc/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-from .models import Doc
+from .models import Doc, TextFile
 from .forms import ImageUpload, TextUpload, AudioUpload, VideoUpload
+from facapp.settings import MEDIA_ROOT
+import os
 
 # Create your views here.
 
@@ -35,5 +37,8 @@ def new_entry(request, btn):
 
 def delete(request, pk):
     doc = Doc.objects.get(pk=pk)
+    if(doc.fileType == '.txt' or doc.fileType == '.md' or doc.fileType == '.pdf'):
+        text = TextFile.objects.get(pk=pk).File
+        os.remove(os.path.join(MEDIA_ROOT, str(text)))
     doc.delete()
     return redirect('url_index')
