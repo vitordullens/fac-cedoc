561c17cfef88acb2e621b25f3a1330835b972174
Vitor Dullens <vitordullens2.0@gmail.com>
2018-06-18 14:45:16 -0300

    colocando tudo em pt-br

--- facapp/accounts/forms.py
+++ facapp/accounts/forms.py
@@ -4,9 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.core.validators import RegexValidator
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.', validators=[RegexValidator(regex=r'^[\w\d\.]*@(?:aluno\.)?unb\.br$', message='Only UnB email allowed.', code='invalid_email'),])
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.', validators=[RegexValidator(regex=r'^[\w\d\.]*@(?:aluno\.)?unb\.br$', message='Só são permitidos emails da UnB.', code='invalid_email'),])
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+        fields = ('username', 'email', 'password1', 'password2', )
