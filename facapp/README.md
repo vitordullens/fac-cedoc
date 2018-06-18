21e8180bbd98fc5283a9a0eff62bdbf9297493db
Vitor Dullens <vitordullens2.0@gmail.com>
2018-06-17 22:55:54 -0300

    adding regex for email; normal way working; only UnB mails are allowed; :smile:

--- facapp/README.md
+++
@@ -1,7 +0,0 @@
-### notes
-Para aceitar apenas emails com o dominio da UnB, deve-se alterar o arquivo `validators.py` abaixo:
-
-
-`/venv/lib/python3.5/site-packages/django/core/validators.py`
-
-Para o arquivo `validators.py` encontrado neste diretório
--- facapp/accounts/forms.py
+++ facapp/accounts/forms.py
@@ -1,11 +1,17 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import RegexValidator
 
+def validateEmail(email):
+    if re.match(r'^[\w\d\.]*@(?:aluno\.)?unb\.br$', email):
+        raise ValidationError(
+            _('Only UnB email allowed'),
+        )
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.', validators=[RegexValidator(regex=r'^[\w\d\.]*@(?:aluno\.)?unb\.br$', message='Only UnB email allowed.', code='invalid_email'),])
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
