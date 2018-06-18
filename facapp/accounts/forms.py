364855c7f9bac3f65ba923efe79b06eee0efa6d2
Vitor Dullens <vitordullens2.0@gmail.com>
2018-06-17 23:31:22 -0300

    validar tipo dos arquivos

--- facapp/accounts/forms.py
+++ facapp/accounts/forms.py
@@ -3,11 +3,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.core.validators import RegexValidator
 
-def validateEmail(email):
-    if re.match(r'^[\w\d\.]*@(?:aluno\.)?unb\.br$', email):
-        raise ValidationError(
-            _('Only UnB email allowed'),
-        )
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
