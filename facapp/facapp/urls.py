293c6e522f442e1d66986d0fbe39c5f991c4e776
Gguidini <giovanni.guidini@gmail.com>
2018-06-27 11:49:44 -0300

    Serving media docs in DEVELOPMENT ENVIRONMENT

--- facapp/facapp/urls.py
+++ facapp/facapp/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 import cedoc.views as cedocViews
 import accounts.views as accountsViews
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.contrib.auth import views as authViews
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -39,3 +42,6 @@ urlpatterns = [
     path('categories/delete/<int:pk>', cedocViews.deleteCategory, name='url_delete_category'),
     path('edit/<int:pk>', cedocViews.edit, name='url_edit'),
 ]
+
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
