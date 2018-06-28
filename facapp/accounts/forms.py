5f2c42fafabeef5ead837ddb7b6fe3a6e69ea144
Vitor Dullens <vitordullens2.0@gmail.com>
2018-06-28 12:33:23 -0300

    adicionando comentarios sobre accounts

--- facapp/accounts/forms.py
+++ facapp/accounts/forms.py
@@ -4,6 +4,11 @@ from django.contrib.auth.forms import UserCreationForm
 from django.core.validators import RegexValidator
 
 class SignUpForm(UserCreationForm):
+    """ Este é o formulário de criação do usuário.
+    Ele utiliza parte do formulário disponibilizado pelo próprio django - 'UserCreationForm' - e o modelo de usuário do mesmo - 'User'.
+    Além do formulário padrão foi adicionado o campo email.
+    Nele existe um texto de ajuda que é mostrado na tela, uma função extra de validação para emails da UnB 'RegexValidator' que possui uma mensagem de erro, caso for inserido errado.
+    """
     email = forms.EmailField(max_length=254, help_text='Obrigatório. Informe um endereço de email válido. Apenas emails da UnB são aceitos', validators=[RegexValidator(regex=r'^[\w\d\.]*@(?:aluno\.)?unb\.br$', message='Só são permitidos emails da UnB.', code='invalid_email'),])
     class Meta:
         model = User
--- facapp/accounts/views.py
+++ facapp/accounts/views.py
@@ -3,8 +3,12 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm
 
-# Create your views here.
 def SignUp(request):
+    """ Nesta função da view é tratado sobre o cadastramento do usuário.
+    Ela recebe o metodo POST, verifica se o formulário foi preenchido corretamente.
+    Se sim, é feito o login automaticamente.
+    Se não, mostra-se uma mensagem de erro conforme qual erro foi detectado e mostra novamente o formulário de registro .
+    """
     data = {}
     if request.method == 'POST':
         form = SignUpForm(request.POST)
